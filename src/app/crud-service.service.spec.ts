a4e5ac3dcc4237f683b9073095f905ebc459b64b
ShivamSARK <shivam.sark@gmail.com>
2021-02-15 17:25:08 +0530

    commit

--- src/app/crud-service.service.spec.ts
+++ src/app/crud-service.service.spec.ts
@@ -1,12 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { CrudServiceService } from './crud-service.service';
 
 describe('CrudServiceService', () => {
+
+  
   let service: CrudServiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+      ],
+    });
     service = TestBed.inject(CrudServiceService);
   });
 
