4e2ddcff00eb6e404bd95fbacf6ee49117f36d98
ShivamSARK <shivam.sark@gmail.com>
2021-02-19 15:34:12 +0530

    c

--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -8,71 +8,71 @@ import { CrudServiceService } from '../crud-service.service';
 })
 export class CrudComponent implements OnInit {
 
-  constructor(private _crudservice:CrudServiceService) { }
-  dataTitle="UX Products";
-  fetching:boolean;
-  @ViewChild('id',{static: false}) id:ElementRef;
-  @ViewChild('name',{static: false}) name:ElementRef;
-  @ViewChild('price',{static: false}) price:ElementRef;
-  
-  editMode:boolean=false;
-  editIndex:number;
-  products = [];
-  allcustomers = [];  
-  onAddProduct(id,name,price){  
-    if(this.editMode){
+  constructor(private _crudservice: CrudServiceService) { }
+  dataTitle = "UX Products";
+  fetching: boolean;
+  @ViewChild('id', { static: false }) id: ElementRef;
+  @ViewChild('name', { static: false }) name: ElementRef;
+  @ViewChild('price', { static: false }) price: ElementRef;
+
+  editMode: boolean = false;
+  editIndex: number;
+  products: any = [];
+  allcustomers = [];
+  onAddProduct(id, name, price) {
+    if (this.editMode) {
       this.products[this.editIndex] = {
-        id:id.value,
-        name:name.value,  
-        price:price.value
+        id: id.value,
+        name: name.value,
+        price: price.value
       }
       this.onSaveProduct();
-      this.editMode=false;
-      this.id.nativeElement.value='';
-      this.name.nativeElement.value='';
-      this.price.nativeElement.value='';
-      
-    }
-    else{
-    this.products.push({
-      id:id.value,
-      name:name.value,
-      price:price.value
+      this.editMode = false;
+      this.id.nativeElement.value = '';
+      this.name.nativeElement.value = '';
+      this.price.nativeElement.value = '';
 
-    })
-    this.id.nativeElement.value='';
-    this.name.nativeElement.value='';
-    this.price.nativeElement.value='';
+    }
+    else {
+      this.products.push({
+        id: id.value,
+        name: name.value,
+        price: price.value
+
+      })
+      this.id.nativeElement.value = '';
+      this.name.nativeElement.value = '';
+      this.price.nativeElement.value = '';
+    }
   }
-  }
-  onDeleteProduct(id){
-    alert ("Do you want to delete this item");
-    this.products.splice(id,1);
+  onDeleteProduct(id) {
+    alert("Do you want to delete this item");
+    this.products.splice(id, 1);
     this.onSaveProduct()
   }
-  onSaveProduct(){
+  onSaveProduct() {
     this._crudservice.saveProducts(this.products).subscribe(
-     
+
     )
   }
-   onFetchProduct(){
-    this.fetching= true;
+  onFetchProduct() {
+    this.fetching = true;
     this._crudservice.fetchProducts().subscribe(
-      (response)=>{
-         const data = JSON.stringify (response)
-         this.products=JSON.parse(data)
-         this.fetching= false;
-        },
-      (err)=> console.log(err)
+      (response) => {
+
+        this.products = response
+        this.fetching = false;
+      },
+      (err) => console.log(err)
     )
   }
 
-  onEditProduct(index:number){
-    this.editMode=true;
-    this.editIndex=index;
-    this.id.nativeElement.value=this.products[index].id;
-    this.name.nativeElement.value=this.products[index].name;
-    this.price.nativeElement.value=this.products[index].price;
+  onEditProduct(index: number) {
+    this.editMode = true;
+    this.editIndex = index;
+    this.id.nativeElement.value = this.products[index].id;
+    this.name.nativeElement.value = this.products[index].name;
+    this.price.nativeElement.value = this.products[index].price;
 
 
   }
